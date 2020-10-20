421bcb0cf1fe6b4054a33a36eb825fc6457d0a61
Şahin Yıldız <yildizsa@itu.edu.tr>
2020-10-21 01:17:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const Discord = require("discord.js"); // imports the discord library
 const fs = require("fs"); // imports the file io library
 
 const client = new Discord.Client(); // creates a discord client
-// const token = fs.readFileSync("token.txt").toString(); // gets your token from the file
 
-// console.log(token);
 
 client.once("ready", () => { // prints "Ready!" to the console once the bot is online
 	console.log("Ready!");
